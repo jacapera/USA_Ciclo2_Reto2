41bd7944618f9b57624e2032bb1f86290683d4fd
Jesus Capera <capera125@gmail.com>
2022-08-30 22:37:03 -0500

    Universidad Sergio Arboleda Ciclo2
    version 1.0

---
+++ src/main/java/com/jcapera/reto2/BaseDatosProductos.java
@@ -0,0 +1,91 @@
+package com.jcapera.reto2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *  En esta clase se crearan todos los métodos para realizar las operaciones del reto 2
+ * @author jesus capera
+ */
+public class BaseDatosProductos {
+
+    private Map<Integer, Producto> listaProductos = new HashMap<>();
+     
+    /**
+     * Metodo constructor donde voy a crear una colección de datos de tipo HashMap que representara mi base datos de productos
+     */
+    public BaseDatosProductos() {
+        listaProductos.put(1, new Producto(1, "Manzanas", 5000, 25));
+        listaProductos.put(2, new Producto(2, "Limones", 2300, 15));
+        listaProductos.put(3, new Producto(3, "Peras", 2700, 33));
+        listaProductos.put(4, new Producto(4, "Arandanos", 9300, 5));
+        listaProductos.put(5, new Producto(5, "Tomates", 2100, 42));
+        listaProductos.put(6, new Producto(6, "Fresas", 4100, 3));
+        listaProductos.put(7, new Producto(7, "Helado", 4500, 41));
+        listaProductos.put(8, new Producto(8, "Galletas", 500, 8));
+        listaProductos.put(9, new Producto(9, "Chocolate", 3500, 80));
+        listaProductos.put(10, new Producto(10, "Jamon", 15000, 10));
+    }
+
+    public boolean verificarExistencia(Producto producto) {
+        boolean resultado = listaProductos.containsKey(producto.getCodigo());
+        return resultado;
+    }
+
+    public void agregar(Producto producto) {
+        listaProductos.put(producto.getCodigo(), producto);
+    }
+
+    public void borrar(Producto producto) {
+        listaProductos.remove(producto.getCodigo());
+    }
+
+    public void actualizar(Producto Producto) {
+        listaProductos.replace(Producto.getCodigo(), Producto);
+    }
+
+    public double inventarioTotal() {
+        double sumatoria = 0;
+        for (Map.Entry<Integer, Producto> i : listaProductos.entrySet()) {
+            sumatoria += i.getValue().getPrecio() * i.getValue().getInventario();
+        }
+        return sumatoria;
+    }
+
+    public void presentarInforme() {
+        System.out.println(precioMayor() + " " + precioMenor() + " " + String.format("%.1f", promedio()));
+    }
+
+    public String precioMayor() {
+        double precioMayor = 0;
+        String nombrePrecioMayor = "";
+        for (Producto i : listaProductos.values()) {
+            if (i.getPrecio() > precioMayor) {
+                precioMayor = i.getPrecio();
+                nombrePrecioMayor = i.getNombre();
+            }
+        }
+        return nombrePrecioMayor;
+    }
+
+    public String precioMenor() {
+        double precioMenor = listaProductos.get(1).getPrecio();
+        String nombrePrecioMenor = listaProductos.get(1).getNombre();
+        for (Producto i : listaProductos.values()) {
+            if (i.getPrecio() < precioMenor) {
+                precioMenor = i.getPrecio();
+                nombrePrecioMenor = i.getNombre();
+            }
+        }
+        return nombrePrecioMenor;
+    }
+
+    public double promedio() {
+        double sumatoria = 0;
+        for (Map.Entry<Integer, Producto> i : listaProductos.entrySet()) {
+            sumatoria += i.getValue().getPrecio();
+        }
+        return sumatoria / listaProductos.size();
+    }
+
+}
---
+++ src/main/java/com/jcapera/reto2/Reto2.java
@@ -0,0 +1,49 @@
+package com.jcapera.reto2;
+
+import java.util.Scanner;
+
+/**
+ *  Casos de prueba:
+ * Entrada: AGREGAR 11 Melon 70 13        /  Salida: 	1015010.0
+ * Entrada: BORRAR 10 Jamon 15000 10      /  Salida:	864100.0
+ * Entrada: ACTUALIZAR 7 Helado 65000 11  /  Salida:	1544600.0
+ * Entrada: BORRAR 14 Maiz 45000 12       /  Salida:    ERROR
+ * Entrada: AGREGAR 11 Maiz 70000 1       /  Salida:    1084100.0
+ * Entrada: ACTUALIZAR 10 Jamon 500 10    /  Salida:    869100.0
+ * Entrada: BORRAR 3 Peras 2700 33        /  Salida:	925000.0
+ * Entrada: BORRAR 15 Papa 1500 10        /  Salida:    ERROR
+ * Entrada: ACTUALIZAR 15 Papa 1500 10    /  Salida:	ERROR
+ * Entrada: AGREGAR 3 Peras 2700 33       /  Salida:	ERROR
+ * @author jesus capera
+ */
+public class Reto2 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        String operacion = scanner.nextLine();
+        String[] ingresoDatos = scanner.nextLine().split(" ");
+        int codigo = Integer.parseInt(ingresoDatos[0]);
+        String nombre = ingresoDatos[1];
+        double precio = Double.parseDouble(ingresoDatos[2]);
+        int inventario = Integer.parseInt(ingresoDatos[3]);
+        
+        Producto nuevoProducto = new Producto(codigo, nombre, precio, inventario);
+        BaseDatosProductos baseOperaciones = new BaseDatosProductos();
+        
+        //Validando operación que se desea ejecutar
+        if(operacion.equals("AGREGAR") && ( !baseOperaciones.verificarExistencia(nuevoProducto))){
+            baseOperaciones.agregar(nuevoProducto);
+            baseOperaciones.presentarInforme();
+        }else if(operacion.equals("BORRAR") && (baseOperaciones.verificarExistencia(nuevoProducto))){
+            baseOperaciones.borrar(nuevoProducto);
+            baseOperaciones.presentarInforme();
+        }else if(operacion.equals("ACTUALIZAR") && (baseOperaciones.verificarExistencia(nuevoProducto))){
+            baseOperaciones.actualizar(nuevoProducto);
+            baseOperaciones.presentarInforme();
+        }
+        else{
+            System.out.println("ERROR");
+        }
+    }
+}
